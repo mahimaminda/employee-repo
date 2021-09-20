dadb45088e2559866fa529375b9f2a0d987ab47d
mahima <mahimaminda>
2021-09-20 23:03:27 +0530

    second commit

--- Course.java
+++ Course.java
@@ -4,8 +4,7 @@ public class Course {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello from git customer");
-		System.out.println("hello from git customer again");
+		System.out.println("hello from git course");
 	}
 
 }
